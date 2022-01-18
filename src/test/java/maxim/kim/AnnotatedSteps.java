9bbf84ec7e9fa891314fa29b89cc68f3a025a111
maxim934 <kimmaxim121@gmail.com>
2022-01-18 10:19:03 +0600

    fixes

--- src/test/java/maxim/kim/AnnotatedSteps.java
+++ src/test/java/maxim/kim/AnnotatedSteps.java
@@ -4,14 +4,15 @@ import maxim.kim.steps.WebSteps;
 import org.junit.jupiter.api.Test;
 
 public class AnnotatedSteps {
-private WebSteps steps = new WebSteps();
-@Test
-public void gitTest(){
-    steps.openPage();
-    steps.findRep("eroshenkoam/allure-example");
-    steps.goToRep("eroshenkoam/allure-example");
-    steps.openTabIssue();
-    steps.checkIssueTab(68);
+    private WebSteps steps = new WebSteps();
 
-}
+    @Test
+    public void gitTest() {
+        steps.openPage();
+        steps.findRep("eroshenkoam/allure-example");
+        steps.goToRep("eroshenkoam/allure-example");
+        steps.openTabIssue();
+        steps.checkIssueTab(68);
+
+    }
 }
--- src/test/java/maxim/kim/SelenideTest.java
+++ src/test/java/maxim/kim/SelenideTest.java
@@ -2,6 +2,8 @@ package maxim.kim;
 
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -14,7 +16,7 @@ public class SelenideTest {
 
     @Test
     public void testGithub() {
-//        SelenideLogger.addListener("allure", new AllureSelenide());
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
         open("https://github.com");
 
